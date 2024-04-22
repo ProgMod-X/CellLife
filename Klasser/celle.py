7b673f3f0b64e3787d712e7328cb0220cf2e9be3
NattHerman <doyerhamn@elev.rogfk.no>
2024-04-22 12:41:42 +0200

    modified celle.tegn()

--- Klasser/celle.py
+++ Klasser/celle.py
@@ -10,5 +10,6 @@ class Celle:
         self.energi = energi
         self.nabolag = nabolag
     
-    def tegn(self, canvas, tile_size):
-        canvas.fill_rect(self, self.pos_x, self.pos_y + tile_size)
+    def tegn(self, canvas, tile_size, rect_size):
+        canvas.fill_style("sky")
+        canvas.fill_rect(self.pos_x * tile_size, self.pos_y * tile_size, rect_size, rect_size)
