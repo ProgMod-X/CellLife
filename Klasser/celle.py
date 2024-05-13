cccf9ade9b4217bdc818194887b414e1d4543ee1
NattHerman <doyerhamn@elev.rogfk.no>
2024-05-13 12:50:36 +0200

    finn_mål håndterer et nabolag med indekser

--- Klasser/celle.py
+++ Klasser/celle.py
@@ -4,6 +4,7 @@ class Celle:
     pos_x: int = None
     pos_y: int = None
     energi: int = None
+    vision = None
 
     mål_x: int = None
     mål_y: int = None
@@ -21,24 +22,22 @@ class Celle:
         self.energi = energi
 
         self.har_mål = False
-        self.mål = [
-            "busk",
-            "celle"
-        ]
 
         self.vekt_max = 1.0
         self.vekt_min = 0.0
-        self.mål_vekter = {}
-        for m in self.mål:
-            self.mål_vekter[m] = random.randrange(self.vekt_min, self.vekt_max)
+        self.celle_prioritet = random.randrange(self.vekt_min, self.vekt_max)
+        # self.mål_vekter = {}
+        # for m in self.mål:
+        #     self.mål_vekter[m] = random.randrange(self.vekt_min, self.vekt_max)
     
     def tegn(self, canvas, tile_size, rect_size):
         canvas.fill_style = "blue"
         canvas.fill_rect(self.pos_x * tile_size, self.pos_y * tile_size, rect_size, rect_size)
     
-    def beveg(self, target_x, target_y):
+    def beveg(self):
         # Hvis cellen er på målet trenger den ikke å bevege seg.
-        if target_x == self.pos_x and target_y == self.pos_y:
+        if self.mål_x == self.pos_x and self.mål_y == self.pos_y:
+            self.har_mål = False
             return [self.pos_x, self.pos_y]
 
         # Cellen kan kun bevege seg på en akse om gangen. F.eks. først x, deretter y
@@ -46,18 +45,18 @@ class Celle:
         # Den kan se på de fire nye mulige posisjonene og velge den som er nermest målet.
 
         x_direction = 1
-        if target_x < self.pos_x:
+        if self.mål_x < self.pos_x:
             x_direction = -1
         
         y_direction = 1
-        if target_y < self.pos_y:
+        if self.mål_y < self.pos_y:
             y_direction = -1
         
         new_x_pos = self.pos_x + x_direction
-        new_x_distance = ((target_x - new_x_pos)**2 + (target_y - self.pos_y)**2)**0.5
+        new_x_distance = ((self.mål_x - new_x_pos)**2 + (self.mål_y - self.pos_y)**2)**0.5
 
         new_y_pos = self.pos_y + y_direction
-        new_y_distance = ((target_x - self.pos_x)**2 + (target_y - new_y_pos)**2)**0.5
+        new_y_distance = ((self.mål_x - self.pos_x)**2 + (self.mål_y - new_y_pos)**2)**0.5
 
         # Beveg funksjonen returnerer den nye posisjonen.
         # En utvendig funksjon bestemmer om denne bevegelsen er mulig.
@@ -66,6 +65,40 @@ class Celle:
         else:
             return [self.pos_x, new_y_pos]
 
-    def finn_mål(self, nabolag):
-        valid_targets = self.mål
-        return
+    def finn_mål(self, nabolag, entities):
+        if self.har_mål:
+            # Check if the target is still there.
+            local_x = self.mål_x - self.pos_x + 2 # 2 is temp for vision range
+            local_y = self.mål_y - self.pos_y + 2
+            if nabolag[local_x][local_y] != None:
+                return 
+
+        valid_targets = []
+        for row in nabolag:
+            for tile in row:
+                if tile != None:
+                    valid_targets.append(entities[tile])
+        
+        if len(valid_targets) == 0:
+            self.mål_x = random.randint(-2, 2) + self.pos_x
+            self.mål_y = random.randint(-2, 2) + self.pos_y
+
+        # Choose target
+        choice = random.random()
+        target = None
+        if choice > self.celle_prioritet:
+            for potential_target in valid_targets:
+                if type(potential_target) == Celle:
+                    target = potential_target
+        else:
+            for potential_target in valid_targets:
+                if type(potential_target) != Celle and potential_target:
+                    target = potential_target
+
+        if target:
+            self.mål_x = target.pos_x
+            self.mål_y = target.pos_y
+        else:
+            self.mål_x = random.randint(-2, 2) + self.pos_x
+            self.mål_y = random.randint(-2, 2) + self.pos_y
+        self.har_mål = True
